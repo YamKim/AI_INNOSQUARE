fb8e62e833287fbe5e787556e8da576ca0856d7e
yamkim <coramdeo0708@gmail.com>
2020-09-15 09:36:55 +0900

    remove django folder

--- AI_square/MyPro/MyApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<h1>안녕하세요. 김예함입니다.<br> \
-            <a href=http://blog.naver.com/ahalinux>블로그 </a> \
-                        <br> <a href=http://algomorgo.tistory.com> 카페 </a>")
